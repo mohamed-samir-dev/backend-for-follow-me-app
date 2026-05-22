7cecf12ef749c2dedfa77489e13cdb7922c0a5ee
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-05-22 03:50:15 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import servicesRouter from "./routes/services.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/services", servicesRouter);
+
+app.get("/", (_, res) => res.json({ status: "ok", message: "✅ API is running!" }));
 
 app.get("/api/health", (_, res) => res.json({ status: "ok" }));
 
